a106cadec08bf6c71a116d53a211f97be98e0e4e
Bharanidharan03 <justinbharani24@gmail.com>
2025-09-17 10:05:51 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// serve static files from "public" folder
-app.use(express.static(path.join(__dirname, "public")));
+// Serve everything inside frontend/ as static files
+app.use(express.static(path.join(__dirname, "frontend")));
 
+// Default route → send index.html
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 app.listen(5000, () => {
   console.log("Website running at http://localhost:5000");
 });
+
